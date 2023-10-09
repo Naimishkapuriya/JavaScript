6c704f8b79f45fdef45df3ebbbae5f45749c1952
Kapuriya Naimish Dineshbhai <131441944+Naimishkapuriya@users.noreply.github.com>
2023-10-09 10:38:56 +0530

    09-10-2023

--- Functions/Functions.js
+++ Functions/Functions.js
@@ -104,13 +104,13 @@
 
 // ========================call stack====================
 
-let val1 =10
-let val2 =5
-function addNum(num1, num2){
-    let total = num1 + num2
-    console.log(total);
-}
-let result1 = addNum(val1, val2)
-let result2 = addNum(10,2)
+// let val1 =10
+// let val2 =5
+// function addNum(num1, num2){
+//     let total = num1 + num2
+//     console.log(total);
+// }
+// let result1 = addNum(val1, val2)
+// let result2 = addNum(10,2)
 // console.log(result1);
 // console.log(result2);
